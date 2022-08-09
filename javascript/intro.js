91cf888e5dba32b9d028a811d6d54788615ca0bd
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-08-08 17:26:06 -0700

    Update intro.js

--- javascript/intro.js
+++ javascript/intro.js
@@ -15,11 +15,57 @@
 // console.log(myVar = 10) -> = is the assignment operator
 // == or === is the comparison operator
 
-let myVar
-console.log(23%5)
+// let myVar
+// console.log(23%5)
 
 // write function to determine odd or even
 
+// let myVar = 3
+
+function isOdd(number) {
+    // const myVar = 10
+    const remainder = number % 2
+    console.log(myVar)
+    if (remainder === 1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// console.log(isOdd(55))
+
+// console.log('my program is done')
 
 
+// javascript objects => collection of key, value pairs that are related for a particular object {}
+const dog = {
+    numOfLegs: 4,
+    numOfLegs: 6,
+    numOfEyes: 2,
+    name: "Sparky",
+    isAlive: true,
+    // 
+    bark: function () {
+        console.log("ruff ruff")
+    }
+}
 
+// console.log(dog.isAlive)
+// console.log(dog.bark())
+
+// const myStr = "asd"
+// console.log("asdsafdsdgdsgsfd".length)
+
+// javascript arrays [] 
+// difference between arrays and objects
+// 1. arrays are not key value pairs
+// 2. inside of an array, the order of items matter - inside objects, the order of key/value pairs do not matter
+// 3. elements (items) inside of arrays do not have to be unique - inside objects, keys have to be unique
+const myEmptyArr = []
+const myArr = ["item1", "item1", 1, true, null, undefined, dog, myEmptyArr, {}, []]
+
+console.log(dog)
+console.log(myArr)
+
+// CRUD on objects and arrays & array methods
