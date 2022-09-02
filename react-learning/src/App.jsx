ea3015cedf456e2ea359186227c71720b79e7adb
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-09-01 17:15:27 -0700

    refactor form

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
 import Tag from "./components/common/Tag"
+import { useState } from 'react';
 
 function App() {
+  const [hideForm, setHideForm] = useState(false)
+
+  const formToggler = () => {
+    setHideForm(prevState => !prevState)
+  }
+
   return (
     <div className="App">
-      <Form />  
+      {!hideForm && <Form title="Register" />}  
+      {/* <Form title="Login" />   */}
+      <button className='border border-black' onClick={formToggler}>Hide Form</button>
       <Tag />      
     </div>
   );
--- react-learning/src/components/Form/index.jsx
+++ react-learning/src/components/Form/index.jsx
@@ -6,60 +6,61 @@ const initialForm = {
     password: ""
 }
 
-function Form () {
+function Form ({title}) {
     const [form, setForm] = useState(initialForm)
-    const [name, setName] = useState("")
-    const [counter, setCounter] = useState(0)
-    
+
     const handleInput = (event) => {
-        setName(event.target.value)
+        setForm((prevForm) => ({...prevForm, [event.target.name]: event.target.value}))
     }
 
-    const increaseCounter = () => {
-        setCounter(counter + 1)
-        for (let i = 1; i < 6; i++) {
-            // setCounter(counter + 1) // setCounter(1)
-            setCounter((prevCounter) => (prevCounter + 1))
+    const inputFields = [
+        {
+           id: 1,
+           name: "name",
+           value: form.name,
+           onChange: handleInput,
+           type: "text"
+        },
+        {
+           id: 2,
+           name: "email",
+           value: form.email,
+           onChange: handleInput,
+           type: "email"
+        },
+        {
+           id: 3,
+           name: "password",
+           value: form.password,
+           onChange: handleInput,
+           type: "password"
         }
+    ]
+
+    function capFirstLetter (str) {
+        let s = str.split("") // ["e", "m", "a", "i", "l"]
+        s[0] = s[0].toUpperCase()
+        return s.join("")
     }
 
-    const decreaseCounter = () => {
-        // setCounter(counter - 1)
-        for (let i = 1; i < 6; i++) {
-            setCounter(counter - 1)
-        }
-    }
+    console.log(form)
 
     return (
         <>
-           
-
             <form className="flex flex-col border border-black mt-4 w-full md:w-6/12 lg:w-3/12 mx-auto p-4 space-y-4">
-                <h1 className="text-3xl"><strong>Register</strong></h1>
-
-                <h2 className='text-3xl'>{counter}</h2>
-            <button type="button" className='border border-black' onClick={increaseCounter}>+5</button>
-            <button type="button" className='border border-black' onClick={decreaseCounter}>-5</button>
+                <h1 className="text-3xl"><strong>{title}</strong></h1>
 
                 <div className="space-y-4">
-                    <label className="flex flex-col text-start">
-                        Name 
-                        <input className="border border-black p-2 rounded-md" onChange={handleInput} />
-                    </label>
-
-                    <label className="flex flex-col text-start">
-                        Email 
-                        <input className="border border-black p-2 rounded-md" />
-                    </label>
-                    
-                    <label className="flex flex-col text-start">
-                        Password 
-                        <input className="border border-black p-2 rounded-md" />
-                    </label>
+                    {inputFields.map(({id, name, type, onChange, value}) => (
+                        <label className="flex flex-col text-start" key={id}>
+                            {capFirstLetter(name)} 
+                            <input className="border border-black p-2 rounded-md" type={type} name={name} onChange={onChange} value={value} />
+                        </label>
+                    ))}
                 </div>
-
-                <button className="bg-blue-500 p-2 text-white rounded-md">Submit</button>
-        </form>
+                <button className="bg-blue-500 p-2 text-white rounded-md">{title === "Register" ? "Submit" : "Sign In"}</button>
+                
+            </form>
         </>
     )
 }
